ff7b383cafaa2fe38619bab617a955d399585257
FPierre <pflauder@gmail.com>
2020-03-30 11:41:42 +0200

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-  // root: true,
   parser: '@typescript-eslint/parser',
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   plugins: [
+    '@typescript-eslint',
     'unicorn',
     'jest',
   ],
