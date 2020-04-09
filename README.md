f30e1b9009c0d4d2f46820b371f3ad63c2dbb4c5
FPierre <pflauder@gmail.com>
2020-04-09 11:16:04 +0200

    Remove unused code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# La Cuisine
+# TS starter
+
+https://gerlacdt.github.io/posts/eslint-prettier-typescript/
+
+
+https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
+https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+https://dev.to/benweiser/how-to-set-up-eslint-typescript-prettier-with-create-react-app-3675
+
+
+https://thesoreon.com/blog/how-to-set-up-eslint-with-typescript-in-vs-code
+https://levelup.gitconnected.com/setting-up-eslint-with-prettier-typescript-and-visual-studio-code-d113bbec9857
+https://github.com/microsoft/vscode-eslint
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import 'module-alias/register';
 
 (() => {
-  console.log('la cuisine');
+  console.log('hello world');
 })();
-
-https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
-https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
-https://dev.to/benweiser/how-to-set-up-eslint-typescript-prettier-with-create-react-app-3675
-
-
-https://thesoreon.com/blog/how-to-set-up-eslint-with-typescript-in-vs-code
-https://levelup.gitconnected.com/setting-up-eslint-with-prettier-typescript-and-visual-studio-code-d113bbec9857
-https://github.com/microsoft/vscode-eslint
