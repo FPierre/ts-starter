da906b1de2c44e4ab34cacc0857ad0752c00d5e9
FPierre <pflauder@gmail.com>
2020-04-20 15:30:54 +0200

    Fix lint issue on import rule for TS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,28 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
     'plugin:jest/recommended',
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'unicorn',
-    'jest',
-  ],
+  plugins: ['@typescript-eslint', 'unicorn', 'jest'],
   env: {
     node: true,
     'jest/globals': true,
+  },
+  rules: {
+    // https://github.com/benmosher/eslint-plugin-import/issues/1615
+    // https://github.com/benmosher/eslint-plugin-import/issues/1718
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    // Broken for TS files
+    'import/no-unresolved': 0,
   },
 };
